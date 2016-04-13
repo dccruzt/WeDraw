829fdd1e4013e7d0a7d239f41bcaa3b3e9742032
Andres Revolledo <aqwssama@gmail.com>
2016-04-13 00:13:08 -0500

    Added Name Inputy activity layout

---
+++ app/src/main/java/upc/edu/pe/wedraw/InputNameActivity.java
@@ -0,0 +1,15 @@
+package upc.edu.pe.wedraw;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by aqws3 on 4/12/16.
+ */
+public class InputNameActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
